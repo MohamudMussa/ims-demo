67e603451fe222ea3d14b1148d22cbf04749f73b
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-01-11 17:22:57 +0000

    updating sql

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,3 +1,4 @@
 create database if not exists ims;
-create table if not exists ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40), address varchar(40));
-create table if not exists ims.product(id int primary key auto_increment, something varchar(40), nothing varchar(40));
+create table if not exists ims.customers(customerid int primary key auto_increment, first_name varchar(40), surname varchar(40), address varchar(40));
+create table if not exists ims.orders(orderid int primary key auto_increment, first_name varchar(40), surname varchar(40), address varchar(40));
+
